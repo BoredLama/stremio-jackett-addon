73fd56933959820ee2dae3f70ae5d3509f5d20b4
BoredLama <43473329+BoredLama@users.noreply.github.com>
2018-09-21 16:16:07 +0300

    Config Helper

---
+++ config.js
@@ -0,0 +1,65 @@
+const commentJson = require('comment-json')
+
+const path = require('path')
+const rootDir = path.dirname(process.execPath)
+
+const fs = require('fs')
+
+const configFile = 'config.json'
+
+const defaultConfig = {
+
+	"// responseTimeout": [["// for stremio add-on, in milliseconds, if timeout is reached it will respond with whatever results it already has, 0 = no timeout"]],
+	"responseTimeout": 11000,
+
+	"// minimumSeeds": [["// remove torrents with less then X seeds"]],
+	"minimumSeeds": 3,
+
+	"// maximumResults": [["// maximum number of torrents to respond with, 0 = no limit"]],
+	"maximumResults": 15,
+
+	"jackett": {
+
+		"// host": [["// self explanatory, the default port is presumed"]],
+		"host": "http://127.0.0.1:9117/",
+
+		"// readTimeout": [["// read timeout in milliseconds for http requests to jackett server, 0 = no timeout"]],
+		"readTimeout": 10000,
+
+		"// openTimeout": [["// open timeout in milliseconds for http requests to jackett server, 0 = no timeout"]],
+		"openTimeout": 10000
+
+	}
+}
+
+const readConfig = () => {
+
+	const configFilePath = path.join(rootDir, configFile)
+
+	if (fs.existsSync(configFilePath)) {
+		var config
+
+		try {
+			config = fs.readFileSync(configFilePath)
+		} catch(e) {
+			// ignore read file issues
+			return defaultConfig
+		}
+
+		return commentJson.parse(config.toString())
+	} else {
+		const configString = commentJson.stringify(defaultConfig, null, 4)
+
+		try {
+			fs.writeFileSync(configFilePath, configString)
+		} catch(e) {
+			// ignore write file issues
+			return defaultConfig
+		}
+
+		return readConfig()
+	}
+
+}
+
+module.exports = readConfig()
